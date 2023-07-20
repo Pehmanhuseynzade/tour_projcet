4258e5f48599d95179136a733b7610da637267be
Pehmanhuseynzade <126595656+Pehmanhuseynzade@users.noreply.github.com>
2023-07-20 11:59:14 +0400

    added contact page

--- client/src/Pages/user/Contact/index.jsx
+++ client/src/Pages/user/Contact/index.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
 import "./contact.scss"
+import {Link} from "react-router-dom"
 function Contact() {
     return (
         <>
             <div className='contact-count'>
-                <img src="https://mksdmcdn-9b59.kxcdn.com/trawell/wp-content/uploads/2018/03/rawpixel-com-191102-unsplash-1920x1254.jpg" alt="mainimage" />
-                <h1>Contact Page</h1>
+                <img src="https://voyage.qodeinteractive.com/wp-content/uploads/2016/04/contact-title-image.jpg" alt="mainimage" />
+                <h1>A Reasonable Place To Visit</h1>
+                <p>INCREDIBLE JOURNEY</p>
+            </div>
+
+            <div className='voyage'>
+                <div><p style={{color:"gray",fontSize:"23px"}}>Voyage / A Reasonable Place To Visit</p></div>
+                <div style={{display:"flex",gap:"10px"}}>
+                    <Link><div><i style={{color:"blue"}} class="fa-brands fa-facebook-f"></i> Facebook</div></Link>
+                    <Link><div><i style={{color:"lightblue"}} class="fa-brands fa-twitter"></i> Twitter</div></Link>
+                </div>
+            </div>
+
+            <div className='contact-iframe'>
+                <div className='contact-part'>
+                <h1>Contact Us</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus</p>
+                <p className='prg'>vitae condimentum egestas, libero dolor auctor.</p>
+                <form>
+                    <input style={{display:"block",flexDirection:"column"}} type="text" />
+                    <input style={{display:"block",flexDirection:"column"}} type="text" />
+                    <input style={{display:"block",flexDirection:"column"}} type="text" />
+                    <textarea style={{display:"block",flexDirection:"column"}} name="message" cols="30" rows="10"></textarea>
+                    <button type='submit'>Submit</button>
+                </form>
+                </div>
+                <div className='iframe-part'>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d26728459.75101552!2d27.561881!3d35.137876!3m2!1i1024!2i768!4f13.1!5e0!3m2!1str!2sus!4v1689767590395!5m2!1str!2sus" style={{ width: 600, height: 400, border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div className='london-baku'>
+                <div className='london'>
+                <h2>London Office</h2>
+                <Link><p>198 West 21th Street, New York</p></Link>
+                <Link><p>Phone: +88 (0) 101 0000 000</p></Link>
+                <Link><p>Fax: +88 (0) 202 0000 001</p></Link>
+                <Link><p>Email: voyage@qodeinteractive.com</p></Link>
+                </div>
+                <div className='baku'>
+                <h2>Baku Office</h2>
+                <Link><p>198 Bakixanov 21th Street, Baku</p></Link>
+                <Link><p>Phone: +88 (0) 101 0000 000</p></Link>
+                <Link><p>Fax: +88 (0) 202 0000 001</p></Link>
+                <Link><p>Email: voyage@qodeinteractive.com</p></Link>
+                </div>
+                </div>
+                </div>
             </div>
         </>
     )
